31fe10b93919f5ff243567bb2ef1420d7b5e964c
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2021-04-07 13:01:19 -0700

    help and list is implemented :)

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import socket as sct
+import os
 
 server_name = "127.0.0.1"
 server_port = 2121
-client_socket = sct.socket(AF_INET, SOCK_DGRAM)
-
-valid_commands = ['HELP', 'LIST', 'DWLD', 'PWD', 'CD']
-def helpClient():
-
+client_socket = sct.socket(sct.AF_INET, sct.SOCK_DGRAM)
 while True:
-    command = input("Input Your Command and Press Enter...")
+    command = input("Enter Command: ")
+    client_socket.sendto(command.encode(), (server_name, server_port))
+    answer, server_address = client_socket.recvfrom(2048)
+    print(answer.decode())
+    client_socket.close()
--- server/server.py
+++ server/server.py
@@ -1,3 +1,31 @@
 import socket as sct
-
+import os
 server_port = 2121
+server_socket = sct.socket(sct.AF_INET, sct.SOCK_DGRAM)
+server_socket.bind(('127.0.0.1', server_port))
+
+#functions
+def ftpHelp():
+    h = "Enter one of the following commands:\n\n    # HELP: 			    Show this help\n    # LIST: 			    List files\n    # DWLD \"file_path\": 	Download file\n    # PWD: 					Show current dir\n    # CD \"dir_name\": 		Change directory\n    # QUIT:					Exit\n"
+    server_socket.sendto(h.encode(), client_address)
+
+def ftpList():
+    ls = ""
+    files_folders = [f for f in os.listdir()]
+    size_of_files_in_current_folder = 0
+    for i in files_folders:
+        if os.path.isdir(i):
+            ls = ls + '>' + i + '(' + str(os.path.getsize(i)) + ' B)' + '\n'
+        if os.path.isfile(i):
+            ls = ls + ' ' + i + '(' + str(os.path.getsize(i)) + ' B)' + '\n'
+            size_of_files_in_current_folder = size_of_files_in_current_folder + os.path.getsize(i)
+    ls = ls + "\nTotal size of files in current directory: " + str(size_of_files_in_current_folder) + " Bytes\n"
+    server_socket.sendto(ls.encode(), client_address)
+
+while True:
+    command, client_address = server_socket.recvfrom(2048)
+    if command.decode() == "HELP":
+        ftpHelp()
+
+    elif command.decode() == "LIST":
+        ftpList()
